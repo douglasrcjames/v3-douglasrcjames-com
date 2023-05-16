44b07e233e0fb18ccfe7cab2c3b9cb0d8921069a
Douglas James <douglasrcjames@gmail.com>
2023-05-16 15:40:18 -0700

    Added baseline images, and api folder ex

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,9 +1,40 @@
 
 export default function Music() {
-  return (
-    <main className="container px-5">
-      <h1>Music</h1>
-      <p>Links to my music channels, but focus will be on Shopify playlist API connection.</p>
-    </main>
-  );
+    const songs = [
+        {
+            id: "0",
+            name: "Beats 123",
+            artist: "Douglas",
+        },
+        {
+            id: "1",
+            name: "Beats 234",
+            artist: "DD",
+        },
+        {
+            id: "2",
+            name: "Beats 345",
+            artist: "CC",
+        },
+    ];
+
+    return (
+        <main className="container px-5">
+            <h1>Music</h1>
+            <p>Links to my music channels, but focus will be on Spotify playlist API connection. This is coming soon!</p>
+            {/* {
+                songs.map((song, i) => {
+                    return (
+                        <div key={i} className="px-5 py-2 rounded border-2 border-blue-500 border my-10">
+                            <h3>Name: {song.name}</h3>
+                            <h4>Artist: {song.artist}</h4>
+                            <p>ID: {song.id}</p>
+                        </div>
+                    )
+                })
+            } */}
+            
+            
+        </main>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Home() {
     return (
         <main className="container px-5">
@@ -12,6 +14,31 @@ export default function Home() {
                 <li><b>+</b> {"In my free time, I'm working on Minute.tech to connect non-techies to techies for tech support."}</li>
             </ul>
             <button className="px-8 py-2 bg-blue-500 text-white rounded border-2 border-blue-500 hover:bg-transparent hover:text-blue-500">Work with me</button>
+
+            {/* <div className="p-3 text-center border-solid" style={{flex: "0 1 100%", flexBasis: "100%"}}>
+                <Image 
+                    width="100"
+                    height="100"
+                    alt="computer"
+                    className="m-3.5 overflow-clip text-center w-24" 
+                    src="/icons/web-basic.png"
+                />
+                    <h3 className="text-[1.80rem] font-bold break-words text-center" style={{wordWrap: "break-word"}}>Basic</h3>
+                <ul className="text-slate-800 leading-[1.60rem] m-4 text-center"><li className="list-item leading-[1.20rem] p-3 text-center"><div className="leading-[1.20rem] text-center">5 responsive web pages</div>
+                <span className="text-[0.75rem] leading-[0.90rem] text-center">We will design your website with up to 5 custom web screens that look great on every device; iPhone, Android, Mac, and more. These pages are often Home, About Us, What We Do, F.A.Q., Products, etc.</span></li>
+                <li className="list-item leading-[1.20rem] p-3 text-center"><div className="leading-[1.20rem] text-center">Contact form</div>
+                <span className="text-[0.75rem] leading-[0.90rem] text-center">Form that collects visitor name, email, message, and automatically sends you an email with this info. Lowers the barrier for potential visitors to contact you with a personal message or question.</span>
+                </li>
+                <li className="list-item leading-[1.20rem] p-3 text-center"><div className="leading-[1.20rem] text-center">Google Analytics</div>
+                <span className="text-[0.75rem] leading-[0.90rem] text-center">Google Analytics will allow you analyze the traffic that visits your website. Collects metrics such as unique visitors, page views, session duration, and more! 
+                An example of this being helpful is if you are trying to understand if your advertisements are properly driving visitors to your website.</span>
+                </li>
+                <li className="list-item leading-[1.20rem] p-3 text-center">
+                    <div className="leading-[1.20rem] text-center">SEO</div>
+                    <span className="text-[0.75rem] leading-[0.90rem] text-center">Search Engine Optimization (SEO) increases your business relevance ranking on any search engine (Google, Bing, etc). 
+                So if someone searches on Google for your business name or related keywords, you will appear at the top. Results can differ depending on your market, business name, competitors, and more.</span>
+                </li></ul>
+                <span className="text-lime-700 text-[1.60rem] text-center">$1000-$2000</span></div> */}
         </main>
     )
 }
